2633849290c689ab06be2fd0e7d74d8ee7c8e624
Gibsonaj9 <gibson.aj9@gmail.com>
2021-10-11 17:31:11 +1100

    jQuery and added about page
    Completed step 3 of assignment and also added missing page About Me.

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+
+
+$(".homelogo").on("click", function(){
+    $(location).attr('href','index.html')
+});
+
+$("#aboutNav").on("click", function(){
+    $(location).attr('href','about.html')
+});
+
+$("#skillsNav").on("click", function(){
+    $(location).attr('href','my_skills.html')
+});
+
+$("#workNav").on("click", function(){
+    $(location).attr('href','my_work.html')
+});
+
+$("#contactNav").on("click", function(){
+    $(location).attr('href','index.html')
+});
+
+$(".aboutButton").on("click", function(){
+    $(location).attr('href','about.html')
+});
+
+$(".casestudyButton").on("click", function(){
+    $(location).attr('href','my_work.html')
+});
+
+$(".download").on("click", function(){
+    $(location).attr('href','index.html')
+});
